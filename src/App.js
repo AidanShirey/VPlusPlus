58f27d350310f6ef00bab85dbeb0d66cd16d4976
Aidan Shirey <aidan.shirey@gmail.com>
2021-05-31 21:55:51 -0700

    React Convert With Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import {Maptab} from './components/Maptab';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My react app</h1>
+      <div className="fullscreen">
+      <div className="topnavbar">
+        <div className="logo"><strong>V++</strong></div>
+        <div className="dividerline">|</div>
+        <div className="topnavtab">
+          <Maptab name="Ascent"/>
+        </div>
+        <div className="topnavtab">
+          <Maptab name="Bind"/>
+        </div>
+        <div className="topnavtab">
+          <Maptab name="Breeze"/>
+        </div>
+        <div className="topnavtab">
+          <Maptab name="Haven"/>
+        </div>
+        <div className="topnavtab">
+          <Maptab name="Icebox"/>
+        </div>
+        <div className="topnavtab">
+          <Maptab name="Split"/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Maptab.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+
+export class Maptab extends Component {
+  render() {
+    return (
+    <div className="dropdown">
+        <button className="dropbtn">{this.props.name}</button>
+        <div className="dropdown-content">
+          <a>Overview</a>
+          <a>Defense</a>
+          <a>Offense</a>
+          <a>Lineups</a>
+        </div>
+    </div>
+    );
+  }
+}
+
+
+
+
