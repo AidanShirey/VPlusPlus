508163cef2cb83bf4c88a2bbe595dfd85f6b4e56
Aidan Shirey <aidan.shirey@gmail.com>
2021-06-05 16:35:03 -0700

    Removed Sample Videos - Access from Youtube

--- src/components/MapSitePage.js
+++ src/components/MapSitePage.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import './MapSitePage.css'
 import SiteCard from './SiteCard'
-import video from './Sova-A-Ascent-Defense/Heaven-Site.mp4'
-import video2 from './Sova-B-Ascent-Defense/CT-MainArch.mp4'
 
 
 export default function MapSitePage(props) {
@@ -13,14 +11,14 @@ export default function MapSitePage(props) {
             </div>
             <hr></hr>
             <br/>
-            <SiteCard name="Heaven to Site" video={video}/>
+            <SiteCard name="Heaven to Site"/>
             <br/>
             <div id="b-site" className="header">
                 <em><strong>{props.name} B Site // {props.side}</strong></em>
             </div>
             <hr></hr>
             <br/>
-            <SiteCard name="CT to Main Arch" video={video2}/>
+            <SiteCard name="CT to Main Arch"/>
             <br/>
         </div>
     )
