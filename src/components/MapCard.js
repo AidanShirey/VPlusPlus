672c3e84ee91935b31a492554e5e2955837ef0e2
Aidan Shirey <aidan.shirey@gmail.com>
2021-06-12 18:01:11 -0700

    Added Map Card Routing

--- src/components/MapCard.js
+++ src/components/MapCard.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './MapCard.css'
+import { Link } from 'react-router-dom';
 
 export default function MapCard(props) {
     var map = "mapcard " + props.name;
+    var linkclassdefending = "defending-" + props.name;
+    var linkclassattacking = "attacking-" + props.name;
     return (
         <div className={map}>
             <div className="mapcardspacing mapbuttoncontainer">
                 <button className="mapbutton"><strong>{props.name}</strong></button>
                 <div className="buttondivider"><strong>//</strong></div>
-                <button className="mapbutton"><strong>Defending</strong></button>
+                <Link className="mapbutton" to={linkclassdefending} style={{textDecoration: "none"}}><strong>Defending</strong></Link>
                 <div className="buttondivider"><strong>//</strong></div>
-                <button className="mapbutton"><strong>Attacking</strong></button>
+                <Link className="mapbutton" to={linkclassattacking} style={{textDecoration: "none"}}><strong>Attacking</strong></Link>
             </div>
         </div>
     )
