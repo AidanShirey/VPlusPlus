fcbc99b3f5c1f89c08d539ed41c2f3419d6e82b8
Aidan Shirey <aidan.shirey@gmail.com>
2021-06-13 20:11:38 -0700

    Added Description to Character Select Component

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -6,8 +6,10 @@ import cypher from './cypherportrait.jpg'
 export default function CharacterSelect() {
     return (
         <div className="charselect">
-            <div className="spacing"><img className="imageportraitcontainer" src={sova} alt="Sova"></img></div>
-            <div className="spacing"><img className="imageportraitcontainer" src={cypher} alt="Cypher"></img></div>
+            <div className="description">Click a Character Portrait to select Lineup Options.</div>
+            <div className="portraits"><div className="spacing"><img className="imageportraitcontainer" src={sova} alt="Sova"></img></div>
+            <div className="spacing"><img className="imageportraitcontainer" src={cypher} alt="Cypher"></img></div></div>
+            
         </div>
     )
 }
