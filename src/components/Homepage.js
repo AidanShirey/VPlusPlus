b9f16c3a58c87dc6c329abcf5b02bd72de4c879e
AidanShirey <aidan.shirey@gmail.com>
2022-01-15 15:03:50 -0800

    CSS Update: Responsiveness

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -11,8 +11,6 @@ export default function Homepage() {
             <div className="item"><MapCard name="Ascent"/></div>
             <div className="item"><MapCard name="Bind"/></div>
             <div className="item"><MapCard name="Breeze"/></div>
-          </div>
-          <div className="row">
             <div className="item"><MapCard name="Haven"/></div>
             <div className="item"><MapCard name="Icebox"/></div>
             <div className="item"><MapCard name="Split"/></div>
--- src/components/MapSitePage.js
+++ src/components/MapSitePage.js
@@ -5,8 +5,55 @@ import CharacterSelect from './CharacterSelect'
 
 
 export default function MapSitePage(props) {
+    if (props.name != "Haven"){
+        return (
+            <div className="mapsitepage">
+                <div className="mapsitepagecard">
+                    <div className="mapsitepagecardstripe1"></div>
+                    <div className="mapsitepagecardstripe2"></div>
+                    <div id="a-site" className="mapsiteheader">
+                        <div className="headercontainer"><div className="mapsitehead"><strong>{props.name} A Site&nbsp;</strong></div><div className="mapsitedivider"><strong>//</strong></div><div className="mapsitetext"><strong>&nbsp;{props.side}</strong></div></div>
+                        <CharacterSelect/>
+                    </div>
+                    <div className="row">
+                        <div className="item">
+                            <SiteCard name="Heaven to Site"/>
+                        </div>
+                        <div className="item">
+                            <SiteCard name="Heaven to Generator"/>
+                        </div>
+                        <div className="item">
+                            <SiteCard name="Heaven to Mid"/>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <div className="mapsitepagecard">
+                    <div className="mapsitepagecardstripe1"></div>
+                    <div className="mapsitepagecardstripe2"></div>
+                    <div id="b-site" className="mapsiteheader">
+                    <div className="headercontainer"><div className="mapsitehead"><strong>{props.name} B Site&nbsp;</strong></div><div className="mapsitedivider"><strong> // </strong></div><div className="mapsitetext"><strong>&nbsp;{props.side}</strong></div></div>
+                        <CharacterSelect/>
+                    </div>
+                    <div className="row">
+                        <div className="item">
+                            <SiteCard name="CT to Main Arch"/>
+                        </div>
+                        <div className="item">
+                            <SiteCard name="CT to Site"/>
+                        </div>
+                        <div className="item">
+                            <SiteCard name="CT to Market"/>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+            </div>
+        )
+    }
+    // If map name is equal to Haven, then return the proper site page. 
     return (
-        <div>
+        <div className="mapsitepage">
             <div className="mapsitepagecard">
                 <div className="mapsitepagecardstripe1"></div>
                 <div className="mapsitepagecardstripe2"></div>
@@ -30,7 +77,7 @@ export default function MapSitePage(props) {
             <div className="mapsitepagecard">
                 <div className="mapsitepagecardstripe1"></div>
                 <div className="mapsitepagecardstripe2"></div>
-                <div id="a-site" className="mapsiteheader">
+                <div id="b-site" className="mapsiteheader">
                 <div className="headercontainer"><div className="mapsitehead"><strong>{props.name} B Site&nbsp;</strong></div><div className="mapsitedivider"><strong> // </strong></div><div className="mapsitetext"><strong>&nbsp;{props.side}</strong></div></div>
                     <CharacterSelect/>
                 </div>
@@ -47,6 +94,27 @@ export default function MapSitePage(props) {
                 </div>
             </div>
             <br/>
+            <div className="mapsitepagecard">
+                <div className="mapsitepagecardstripe1"></div>
+                <div className="mapsitepagecardstripe2"></div>
+                <div id="c-site" className="mapsiteheader">
+                <div className="headercontainer"><div className="mapsitehead"><strong>{props.name} C Site&nbsp;</strong></div><div className="mapsitedivider"><strong> // </strong></div><div className="mapsitetext"><strong>&nbsp;{props.side}</strong></div></div>
+                    <CharacterSelect/>
+                </div>
+                <div className="row">
+                    <div className="item">
+                        <SiteCard name="Window to Default"/>
+                    </div>
+                    <div className="item">
+                        <SiteCard name="Window to Back-site"/>
+                    </div>
+                    <div className="item">
+                        <SiteCard name="Mid to A-side plant"/>
+                    </div>
+                </div>
+            </div>
+            <br/>
         </div>
     )
+    
 }
