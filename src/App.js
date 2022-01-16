46e67b2e3eac9b373cdf1cbe81124fbe6fd6f02f
AidanShirey <aidan.shirey@gmail.com>
2022-01-15 23:07:58 -0800

    Routing Test Fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {NavBar} from './components/NavBar'
 import Homepage from './components/Homepage'
 import MapSitePage from './components/MapSitePage'
 import ScrollToTop from './components/ScrollToTop'
-import {BrowserRouter as Router, Route, Switch, withRouter} from "react-router-dom"
+import {HashRouter as Router, Route, Switch, withRouter} from "react-router-dom"
 
 
 
@@ -17,20 +17,20 @@ function App() {
           <ScrollToTop>
           <Switch>
             <Route path="/" exact component={Homepage}/>
-            <Route path="/defending-ascent" component={() => <MapSitePage name="Ascent" side="Defending"/>}/>
-            <Route path="/attacking-ascent" component={() => <MapSitePage name="Ascent" side="Attacking"/>}/>
-            <Route path="/defending-bind" component={() => <MapSitePage name="Bind" side="Defending"/>}/>
-            <Route path="/attacking-bind" component={() => <MapSitePage name="Bind" side="Attacking"/>}/>
-            <Route path="/defending-breeze" component={() => <MapSitePage name="Breeze" side="Defending"/>}/>
-            <Route path="/attacking-breeze" component={() => <MapSitePage name="Breeze" side="Attacking"/>}/>
-            <Route path="/defending-fracture" component={() => <MapSitePage name="Fracture" side="Defending"/>}/>
-            <Route path="/attacking-fracture" component={() => <MapSitePage name="Fracture" side="Attacking"/>}/>
-            <Route path="/defending-haven" component={() => <MapSitePage name="Haven" side="Defending"/>}/>
-            <Route path="/attacking-haven" component={() => <MapSitePage name="Haven" side="Attacking"/>}/>
-            <Route path="/defending-icebox" component={() => <MapSitePage name="Icebox" side="Defending"/>}/>
-            <Route path="/attacking-icebox" component={() => <MapSitePage name="Icebox" side="Attacking"/>}/>
-            <Route path="/defending-split" component={() => <MapSitePage name="Split" side="Defending"/>}/>
-            <Route path="/attacking-split" component={() => <MapSitePage name="Split" side="Attacking"/>}/>
+            <Route path="/defending-Ascent" component={() => <MapSitePage name="Ascent" side="Defending"/>}/>
+            <Route path="/attacking-Ascent" component={() => <MapSitePage name="Ascent" side="Attacking"/>}/>
+            <Route path="/defending-Bind" component={() => <MapSitePage name="Bind" side="Defending"/>}/>
+            <Route path="/attacking-Bind" component={() => <MapSitePage name="Bind" side="Attacking"/>}/>
+            <Route path="/defending-Breeze" component={() => <MapSitePage name="Breeze" side="Defending"/>}/>
+            <Route path="/attacking-Breeze" component={() => <MapSitePage name="Breeze" side="Attacking"/>}/>
+            <Route path="/defending-Fracture" component={() => <MapSitePage name="Fracture" side="Defending"/>}/>
+            <Route path="/attacking-Fracture" component={() => <MapSitePage name="Fracture" side="Attacking"/>}/>
+            <Route path="/defending-Haven" component={() => <MapSitePage name="Haven" side="Defending"/>}/>
+            <Route path="/attacking-Haven" component={() => <MapSitePage name="Haven" side="Attacking"/>}/>
+            <Route path="/defending-Icebox" component={() => <MapSitePage name="Icebox" side="Defending"/>}/>
+            <Route path="/attacking-Icebox" component={() => <MapSitePage name="Icebox" side="Attacking"/>}/>
+            <Route path="/defending-Split" component={() => <MapSitePage name="Split" side="Defending"/>}/>
+            <Route path="/attacking-Split" component={() => <MapSitePage name="Split" side="Attacking"/>}/>
           </Switch>
           </ScrollToTop>
         </div>
